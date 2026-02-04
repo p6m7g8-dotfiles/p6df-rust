d5c6ae2e536bae9cb6c4ec76346677519a79e223
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-02-03 21:37:38 -0500

    chore: regen

--- init.zsh
+++ init.zsh
@@ -31,10 +31,7 @@ p6df::modules::rust::vscodes() {
 ######################################################################
 #<
 #
-# Function: str json = p6df::modules::rust::vscodes::config()
-#
-#  Returns:
-#	str - json
+# Function: p6df::modules::rust::vscodes::config()
 #
 #>
 ######################################################################
